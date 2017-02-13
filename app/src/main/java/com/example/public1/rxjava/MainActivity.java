9760326b33c20a502ec2c9a1369c2ded5e9cb275
jessear <13262623319@163.com>
2017-02-13 21:23:19 +0800

    调度器

--- app/src/main/java/com/example/public1/rxjava/MainActivity.java
+++ app/src/main/java/com/example/public1/rxjava/MainActivity.java
@@ -2,19 +2,28 @@ package com.example.public1.rxjava;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
+import android.widget.Toast;
+
 import org.reactivestreams.Publisher;
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends Activity {
 
@@ -68,68 +77,56 @@ public class MainActivity extends Activity {
 //            }
 //        });
 
-//        3.flatMap()对象类型转换成Observable对象
-        List<Student> students = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            Student student = new Student();
-            student.setName("学生" + i);
-            student.setAge(i);
-            List<String> courses = new ArrayList<String>();
-            for (int j = 0; j < 3; j++) {
-                courses.add(student.getName() + j);
-            }
-            student.setCourses(courses);
-            students.add(student);
-        }
-
-        Flowable.just(students)
-                .flatMap(new Function<List<Student>, Publisher<Student>>() {
-                    @Override
-                    public Publisher<Student> apply(List<Student> students) throws Exception {
-                        return Flowable.fromIterable(students);
-                    }
-                }).flatMap(new Function<Student, Publisher<String>>() {
-            @Override
-            public Publisher<String> apply(Student student) throws Exception {
-                return Flowable.fromIterable(student.getCourses());
-            }
-        }).subscribe(new Consumer<String>() {
-            @Override
-            public void accept(String s) throws Exception {
-                Log.i("---------------->", "课程:" + s);
-            }
-        });
-
-//        Observable.create(new Observable.OnSubscribe<Drawable>() {
-//
-//            public void call(Subscriber<? super Drawable> subscriber) {
-//                Drawable drawable = getTheme().getDrawable(0);
-//                subscriber.onNext(drawable);
-//                subscriber.onComplete();
+//        3.flatMap()
+//        List<Student> students = new ArrayList<>();
+//        for (int i = 0; i < 2; i++) {
+//            Student student = new Student();
+//            student.setName("学生" + i);
+//            student.setAge(i);
+//            List<String> courses = new ArrayList<String>();
+//            for (int j = 0; j < 3; j++) {
+//                courses.add(student.getName() + j);
 //            }
-//        })
-//            .subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
-//            .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程
-//            .subscribe(new Observer<Drawable>() {
-//                @Override
-//                public void onSubscribe(Disposable d) {
+//            student.setCourses(courses);
+//            students.add(student);
+//        }
 //
-//                }
-//
-//                @Override
-//                public void onNext(Drawable drawable) {
-//                }
-//
-//                @Override
-//                public void onError(Throwable e) {
-//
-//                }
-//
-//                @Override
-//                public void onComplete() {
-//
-//                }
-//            });
+//        Flowable.just(students)
+//                .flatMap(new Function<List<Student>, Publisher<Student>>() {
+//                    @Override
+//                    public Publisher<Student> apply(List<Student> students) throws Exception {
+//                        return Flowable.fromIterable(students);
+//                    }
+//                }).flatMap(new Function<Student, Publisher<String>>() {
+//            @Override
+//            public Publisher<String> apply(Student student) throws Exception {
+//                return Flowable.fromIterable(student.getCourses());
+//            }
+//        }).subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Exception {
+//                Log.i("--------------->", "课程:" + s);
+//            }
+//        });
+
+//        4.调度器
+        Flowable.create(new FlowableOnSubscribe<String>() {
+            @Override
+            public void subscribe(FlowableEmitter<String> e) throws Exception {
+                e.onNext("将会在3秒后显示");
+                SystemClock.sleep(3000);
+                e.onNext("已显示");
+                e.onComplete();
+            }
+        }, BackpressureStrategy.BUFFER)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Toast.makeText(MainActivity.this,s,Toast.LENGTH_SHORT).show();
+                    }
+                });
 
     }
 }
