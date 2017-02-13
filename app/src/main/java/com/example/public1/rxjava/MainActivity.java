95bce9b1664a6befb02d2777ca7d15eb62cfae18
jessear <13262623319@163.com>
2017-02-13 18:19:14 +0800

    RxJava2.0

--- app/src/main/java/com/example/public1/rxjava/MainActivity.java
+++ app/src/main/java/com/example/public1/rxjava/MainActivity.java
@@ -4,13 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.reactivestreams.Publisher;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import rx.Observable;
-import rx.Subscriber;
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import rx.functions.Action1;
-import rx.functions.Func1;
 
 public class MainActivity extends Activity {
 
@@ -19,77 +27,113 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        1.简单用法
-//        Observable<String> observable=Observable.create(new Observable.OnSubscribe<String>(){
-//
+//        Observable<String> observable=Observable.create(new ObservableOnSubscribe<String>(){
 //            @Override
-//            public void call(Subscriber<? super String> subscriber) {
-//                subscriber.onNext("what a fuck");
-//                subscriber.onCompleted();
+//            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+//                emitter.onNext("what a fuck");
+//                emitter.onComplete();
 //            }
 //        });
-//
-//        Subscriber<String> subscriber=new Subscriber<String>() {
-//
+//        Observer<String> observer= new Observer<String>() {
 //            @Override
-//            public void onStart() {
-//                Log.i("---------------->", "onStart()");
+//            public void onSubscribe(Disposable d) {
+//                Log.i("---------------->", "onSubscribe()");
 //            }
 //
 //            @Override
-//            public void onCompleted() {
-//                Log.i("---------------->", "onCompleted()");
+//            public void onNext(String value) {
+//                Log.i("---------------->", "onNext()");
 //            }
 //
 //            @Override
 //            public void onError(Throwable e) {
-//
+//                Log.i("---------------->", "onError()");
 //            }
 //
 //            @Override
-//            public void onNext(String s) {
-//                Log.i("---------------->", "onNext(),"+s);
+//            public void onComplete() {
+//                Log.i("---------------->", "onComplete()");
 //            }
 //        };
-//
-//        observable.subscribe(subscriber);
+//        observable.subscribe(observer);
 
 //        2.map()对象类型转换
-//        Observable.just("hello word").map(new Func1<String,Integer>() {
+
+//        Observable.just("hello word").map(new Function<String, Integer>() {
 //            @Override
-//            public Integer call(String s) {
+//            public Integer apply(String s) throws Exception {
 //                return s.length();
 //            }
-//        }).subscribe(new Action1<Integer>() {
+//
+//        }).subscribe(new Consumer<Integer>() {
 //            @Override
-//            public void call(Integer integer) {
+//            public void accept(Integer integer) throws Exception {
 //                Log.i("---------------->", "length:" + integer);
 //            }
 //        });
 
 //        3.flatMap()对象类型转换成Observable对象
-//        List<Student> students=new ArrayList<>();
-//        for(int i=0;i<2;i++){
-//            Student student=new Student();
-//            student.setName("学生" + i);
-//            student.setAge(i);
-//            List<String> courses=new ArrayList<String>();
-//            for(int j=0;j<3;j++){
-//                courses.add(student.getName()+j);
+        List<Student> students = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            Student student = new Student();
+            student.setName("学生" + i);
+            student.setAge(i);
+            List<String> courses = new ArrayList<String>();
+            for (int j = 0; j < 3; j++) {
+                courses.add(student.getName() + j);
+            }
+            student.setCourses(courses);
+            students.add(student);
+        }
+
+        Flowable.just(students)
+                .flatMap(new Function<List<Student>, Publisher<Student>>() {
+                    @Override
+                    public Publisher<Student> apply(List<Student> students) throws Exception {
+                        return Flowable.fromIterable(students);
+                    }
+                }).flatMap(new Function<Student, Publisher<String>>() {
+            @Override
+            public Publisher<String> apply(Student student) throws Exception {
+                return Flowable.fromIterable(student.getCourses());
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.i("---------------->", "课程:" + s);
+            }
+        });
+
+//        Observable.create(new Observable.OnSubscribe<Drawable>() {
+//
+//            public void call(Subscriber<? super Drawable> subscriber) {
+//                Drawable drawable = getTheme().getDrawable(0);
+//                subscriber.onNext(drawable);
+//                subscriber.onComplete();
 //            }
-//            student.setCourses(courses);
-//            students.add(student);
-//        }
-//        Observable.from(students).flatMap(new Func1<Student, Observable<String>>() {
-//            @Override
-//            public Observable<String> call(Student student) {
-//                return Observable.from(student.getCourses());
-//            }
-//        }).subscribe(new Action1<String>() {
-//            @Override
-//            public void call(String s) {
-//                Log.i("---------------->", "课程:" +s );
-//            }
-//        });
+//        })
+//            .subscribeOn(Schedulers.io()) // 指定 subscribe() 发生在 IO 线程
+//            .observeOn(AndroidSchedulers.mainThread()) // 指定 Subscriber 的回调发生在主线程
+//            .subscribe(new Observer<Drawable>() {
+//                @Override
+//                public void onSubscribe(Disposable d) {
+//
+//                }
+//
+//                @Override
+//                public void onNext(Drawable drawable) {
+//                }
+//
+//                @Override
+//                public void onError(Throwable e) {
+//
+//                }
+//
+//                @Override
+//                public void onComplete() {
+//
+//                }
+//            });
 
     }
 }
