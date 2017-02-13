a1b35d949ba49dc20da11f76c45f9d69f2dd8fba
jessear <13262623319@163.com>
2017-02-13 15:19:30 +0800

    map()

--- app/src/main/java/com/example/public1/rxjava/MainActivity.java
+++ app/src/main/java/com/example/public1/rxjava/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends Activity {
 
@@ -13,38 +15,50 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Observable<String> observable=Observable.create(new Observable.OnSubscribe<String>(){
+//        Observable<String> observable=Observable.create(new Observable.OnSubscribe<String>(){
+//
+//            @Override
+//            public void call(Subscriber<? super String> subscriber) {
+//                subscriber.onNext("what a fuck");
+//                subscriber.onCompleted();
+//            }
+//        });
+//
+//        Subscriber<String> subscriber=new Subscriber<String>() {
+//
+//            @Override
+//            public void onStart() {
+//                Log.i("---------------->", "onStart()");
+//            }
+//
+//            @Override
+//            public void onCompleted() {
+//                Log.i("---------------->", "onCompleted()");
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(String s) {
+//                Log.i("---------------->", "onNext(),"+s);
+//            }
+//        };
+//
+//        observable.subscribe(subscriber);
 
+        Observable.just("hello word").map(new Func1<String,Integer>() {
             @Override
-            public void call(Subscriber<? super String> subscriber) {
-                subscriber.onNext("what a fuck");
-                subscriber.onCompleted();
+            public Integer call(String s) {
+                return s.length();
+            }
+        }).subscribe(new Action1<Integer>() {
+            @Override
+            public void call(Integer integer) {
+                Log.i("---------------->","length:"+integer);
             }
         });
-
-        Subscriber<String> subscriber=new Subscriber<String>() {
-
-            @Override
-            public void onStart() {
-                Log.i("---------------->", "onStart()");
-            }
-
-            @Override
-            public void onCompleted() {
-                Log.i("---------------->", "onCompleted()");
-            }
-
-            @Override
-            public void onError(Throwable e) {
-
-            }
-
-            @Override
-            public void onNext(String s) {
-                Log.i("---------------->", "onNext(),"+s);
-            }
-        };
-
-        observable.subscribe(subscriber);
     }
 }
